7807168a230f4a447760afb4249ede6e91f6d33b
jacqschweiger <jacqschweiger@gmail.com>
2016-10-02 20:54:28 -0400

    Done

--- StructLove/PizzaDeliveryService.swift
+++ StructLove/PizzaDeliveryService.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+
+struct PizzaDeliveryService {
+    var location: Coordinate
+    var pizzasAvailable: Int = 10
+    
+    init(location: Coordinate){
+        self.location = location
+    }
+
+    func isInRange(to destination: Coordinate) -> Bool {
+        if destination.distance(to: location) <= 5000 {
+            return true
+        } else {
+            return false
+        }
+    }
+ 
+    mutating func deliverPizza(to destination: Coordinate) -> Bool {
+        if pizzasAvailable > 0 {
+            if isInRange(to: destination) {
+            pizzasAvailable -= 1
+            return true
+        } else {
+            return false
+        }
+        }
+        else {
+            return false
+        }
+    }
+    
+
+}
